e87fd1d47bbd1c70ad3f4fce4a552852228b8894
Stefan Nikolei <stefan@nikolei.eu>
2026-08-23 14:12:00 +0200

    Simplify ToHex using bitwise ops and ToString

--- XlsxSharp/Extensions/ColorExtensions.cs
+++ XlsxSharp/Extensions/ColorExtensions.cs
@@ -1,54 +1,20 @@
 #nullable disable
 
 using System.Drawing;
+using System.Globalization;
 
 namespace XlsxSharp.Extensions;
 
 internal static class ColorExtensions
 {
-    private static readonly char[] hexDigits =
-    [
-        '0',
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        'A',
-        'B',
-        'C',
-        'D',
-        'E',
-        'F',
-    ];
-
+    /// <summary>
+    /// Converts a <see cref="Color"/> to a hexadecimal string representation in the format "AARRGGBB".
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns></returns>
     public static string ToHex(this Color color)
     {
-        byte[] bytes = new byte[4];
-
-        bytes[0] = color.A;
-
-        bytes[1] = color.R;
-
-        bytes[2] = color.G;
-
-        bytes[3] = color.B;
-
-        char[] chars = new char[bytes.Length * 2];
-
-        for (int i = 0; i < bytes.Length; i++)
-        {
-            int b = bytes[i];
-
-            chars[i * 2] = hexDigits[b >> 4];
-
-            chars[i * 2 + 1] = hexDigits[b & 0xF];
-        }
-
-        return new string(chars);
+        uint hexOrder = (uint)((color.B << 0) | (color.G << 8) | (color.R << 16) | (color.A << 24));
+        return hexOrder.ToString("X8", CultureInfo.InvariantCulture);
     }
 }
